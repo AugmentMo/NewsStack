666a828562f6439506909b7ec61081101040ef2e
AugmentMo <moritz@ahlab.org>
2023-04-27 01:15:57 +1200

    moved to SocketIO

--- index.js
+++ index.js
@@ -1,59 +1,77 @@
-const express = require('express');
 const fetch = require('node-fetch');
 const { DOMParser } = require('xmldom');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const { getNewsFeedImage, getMetaFeedData, fetchAndCropImage } = require('./fetchtest');
+const { getMetaFeedData, fetchAndCropImage } = require('./fetchtest');
 
+const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// Define a route to retrieve the news feeds
-app.get('/news', (req, res) => {
-  const url = 'https://news.google.com/rss/search?q=haptics&hl=en-NZ&gl=NZ&ceid=NZ:en';
-  fetch(url)
-    .then(response => response.text())
-    .then(async data => {
-      const parser = new DOMParser();
-      const xml = parser.parseFromString(data, 'text/xml');
-      const items = xml.getElementsByTagName('item');
-        const feeds = [];
-        const maxitem = 5; //items.length;
-      for (let i = 0; i < maxitem; i++) {
-        const title = items[i].getElementsByTagName('title')[0].textContent;
-        const description = items[i].getElementsByTagName('description')[0].textContent;
-        var linkurl = items[i].getElementsByTagName('link')[0].textContent;
-          
 
-        //////
-        var imagesrc = ""
-        var metadescr = ""
-        await getMetaFeedData(linkurl)
-            .then(metadata => {
-                if (metadata != undefined) {
-                    if (metadata["m_description"]) {
-                        metadescr = metadata["m_description"]
-                    }
-                    if (metadata["m_url"]) {
-                        linkurl = metadata["m_url"]
-                    }
-                    if (metadata["m_img"]) {
-                        return fetchAndCropImage(metadata["m_img"]);
-                    }
-                }
-            })
-            .then(imagedata => {imagesrc = imagedata}).catch(error => console.log("could not fetch"));
-        // await getNewsFeedImage(linkurl).then(imagedata => {imagesrc = imagedata}).catch(error => console.log("could not fetch"));
-        //////
-
-        feeds.push({ title, description, linkurl, metadescr, imagesrc });
-      }
-      res.json(feeds);
-    })
-    .catch(error => {
-      console.log(error);
-      res.status(500).send('Error retrieving news feeds');
+io.on("connection", (socket) => {
+    console.log("socket connected")
+    socket.on("getnews", (newscolumn) => {
+        console.log("news request")
+        collectNews(socket, newscolumn)
     });
 });
+  
+
+function collectNews(socket, newscolumn) {
+    const url = 'https://news.google.com/rss/search?q=haptics&hl=en-NZ&gl=NZ&ceid=NZ:en';
+    fetch(url)
+      .then(response => response.text())
+      .then(async data => {
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(data, 'text/xml');
+        const items = xml.getElementsByTagName('item');
+          const feeds = [];
+          const maxitem = 1; //items.length;
+        for (let i = 0; i < maxitem; i++) {
+          const title = items[i].getElementsByTagName('title')[0].textContent;
+          const description = items[i].getElementsByTagName('description')[0].textContent;
+          var linkurl = items[i].getElementsByTagName('link')[0].textContent;
+            
+  
+          //////
+          var imagesrc = ""
+          var metadescr = ""
+          await getMetaFeedData(linkurl)
+              .then(metadata => {
+                  if (metadata != undefined) {
+                      if (metadata["m_description"]) {
+                          metadescr = metadata["m_description"]
+                      }
+                      if (metadata["m_url"]) {
+                          linkurl = metadata["m_url"]
+                      }
+                      if (metadata["m_img"]) {
+                          return fetchAndCropImage(metadata["m_img"]);
+                      }
+                  }
+              })
+              .then(imagedata => {imagesrc = imagedata}).catch(error => console.log("could not fetch"));
+          // await getNewsFeedImage(linkurl).then(imagedata => {imagesrc = imagedata}).catch(error => console.log("could not fetch"));
+          //////
+  
+          feeds.push({ title, description, linkurl, metadescr, imagesrc });
+        }
+        
+        for (const feeditem of feeds) {
+            socket.emit('newsfeeditem', feeditem);
+        }
+
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).send('Error retrieving news feeds');
+      });
+  
+}
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
@@ -64,7 +82,8 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+  
+  
--- public/js/newsfeedhandler.js
+++ public/js/newsfeedhandler.js
@@ -1,68 +1,66 @@
 
-console.log("test")
+console.log("request news")
 
-fetch('/news')
-  .then(response => response.json())
-  .then(data => {
+socket.emit("getnews");
+
+socket.on('newsfeeditem', (feed) => {
+    // Handle news data here
+    console.log("received item: ", feed)
     const container = document.getElementById('news-container-haptics');
-    data.forEach(feed => {
-      console.log(feed)
-      const title = document.createElement('p');
-      title.classList.add("card-title");
-      title.innerHTML = feed.title;
 
-      const description = document.createElement('p');
-      description.classList.add("font-weight-500")
-      description.innerHTML = feed.metadescr;
+    const title = document.createElement('p');
+    title.classList.add("card-title");
+    title.innerHTML = feed.title;
 
-      const url = document.createElement('a');
-      url.classList.add("font-weight-500")
-      url.innerHTML = 'Read more';
-      url.href = feed.linkurl;
-      url.target = '_blank';
+    const description = document.createElement('p');
+    description.classList.add("font-weight-500")
+    description.innerHTML = feed.metadescr;
 
-      const imageContainer = document.createElement('div');
-      imageContainer.classList.add('col');
-      imageContainer.classList.add('d-flex');
-      // imageContainer.classList.add('justify-content-end');
-      imageContainer.classList.add('align-self-center');
-      if (feed.imagesrc != undefined) {
-        imageContainer.innerHTML = '<img src="' + feed.imagesrc + '" alt="' + feed.title + '" style="width: 100px; height: 100px;">';
-      }
+    const url = document.createElement('a');
+    url.classList.add("font-weight-500")
+    url.innerHTML = 'Read more';
+    url.href = feed.linkurl;
+    url.target = '_blank';
 
-      const textContainer = document.createElement('div');
-      textContainer.classList.add('col');
-      textContainer.classList.add('col-lg-8');
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('col');
+    imageContainer.classList.add('d-flex');
+    // imageContainer.classList.add('justify-content-end');
+    imageContainer.classList.add('align-self-center');
+    if (feed.imagesrc != undefined) {
+    imageContainer.innerHTML = '<img src="' + feed.imagesrc + '" alt="' + feed.title + '" style="width: 100px; height: 100px;">';
+    }
 
-
-      const cardBodyElement = document.createElement('div');
-      cardBodyElement.classList.add('card-body');
-
-      cardBodyElement.appendChild(title);
-      cardBodyElement.appendChild(description);
-      cardBodyElement.appendChild(url);
-
-      textContainer.appendChild(cardBodyElement);
-
-      const rowElement = document.createElement('div');
-      rowElement.classList.add('row');
-      rowElement.classList.add('justify-content-between');
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('col');
+    textContainer.classList.add('col-lg-8');
 
 
-      const feedElement = document.createElement('p');
-      feedElement.href = feed.linkurl;
-      feedElement.target = '_blank';
-      feedElement.classList.add('card');
-      feedElement.classList.add('m-2');
+    const cardBodyElement = document.createElement('div');
+    cardBodyElement.classList.add('card-body');
 
-      rowElement.appendChild(textContainer);
-      rowElement.appendChild(imageContainer);
+    cardBodyElement.appendChild(title);
+    cardBodyElement.appendChild(description);
+    cardBodyElement.appendChild(url);
 
-      feedElement.appendChild(rowElement);
+    textContainer.appendChild(cardBodyElement);
 
-      container.appendChild(feedElement);
-    });
-  })
-  .catch(error => console.log(error));
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+    rowElement.classList.add('justify-content-between');
 
-console.log("test")
+
+    const feedElement = document.createElement('p');
+    feedElement.href = feed.linkurl;
+    feedElement.target = '_blank';
+    feedElement.classList.add('card');
+    feedElement.classList.add('m-2');
+
+    rowElement.appendChild(textContainer);
+    rowElement.appendChild(imageContainer);
+
+    feedElement.appendChild(rowElement);
+
+    container.appendChild(feedElement);
+
+});
