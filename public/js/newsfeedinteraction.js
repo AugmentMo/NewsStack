62df756e2d5124713596140862a167cd83d8903d
AugmentMo <moritz@ahlab.org>
2023-04-28 02:14:06 +1200

    allowing to delete stacks

--- public/js/newsfeedinteraction.js
+++ public/js/newsfeedinteraction.js
@@ -35,9 +35,47 @@ $("#addnewsstackbtn").on("click", (event) => {
         label: "OK",
         className: 'btn-dark',
         callback: function() {
-        console.log('Custom OK clicked');
+            console.log('Custom OK clicked');
         }
         }
         }
         });
 });
+
+
+function registerButtonEventListener() {
+    $(".btn-newscol-trash").on("click", (event) => {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        const stackid = $(event.currentTarget).attr('data-stack-id');
+    
+        let dialog = bootbox.dialog({
+            title: 'Delete Stack',
+            message: `
+    <div>
+        <p>Do you really want to delete this stack?</p>
+    </div>
+    
+            `,
+            size: 'medium',
+            buttons: {
+            cancel: {
+            label: "Cancel",
+            className: 'btn-outline-dark',
+            callback: function(){
+            console.log('Custom cancel clicked');
+            }
+            },
+            ok: {
+            label: "Delete",
+            className: 'btn-dark',
+            callback: function() {
+                console.log('Deleting '+stackid);
+                deleteFeed(stackid);
+                updateNewsFeedsDisplay();
+            }
+            }
+            }
+            });
+    });
+}
