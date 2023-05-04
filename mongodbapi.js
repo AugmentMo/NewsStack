8a3ceab09ba435cd54f5ae9fb877e64d16fd0ef8
AugmentMo <moritz@ahlab.org>
2023-05-05 11:01:08 +1200

    connect + create user mongodb

--- mongodbapi.js
+++ mongodbapi.js
@@ -1,5 +1,43 @@
 const { MongoClient } = require('mongodb');
 
+// Connection URI for MongoDB
+const mongodbUri = 'mongodb://localhost:27017';
+
+// Database name
+const dbName = 'newsstack';
+
+// Create a new MongoClient
+const mongodbClient = new MongoClient(mongodbUri);
+
+var mongodbconnected = false;
+var userscollection = null;
+
+function isMongoDBConnected() {
+    return mongodbconnected;
+}
+
+function connectMongoDB() {
+    try {
+        // Connect to the MongoDB server
+        mongodbClient.connect();
+    
+        // Get a reference to the "newsstack" database
+        const db = mongodbClient.db(dbName);
+    
+        // Create a new "users" collection
+        userscollection = db.collection('users');
+        mongodbconnected = true;
+    } catch (error) {
+        mongodbconnected = false;
+    }
+}
+
+function closeMongoDBConnection() {
+    // Close the client connection
+    mongodbClient.close();
+    mongodbconnected = false;
+}
+
 function isUserExisting(sub) {
     
 }
@@ -13,7 +51,18 @@ function updateUserData(sub, usr_data) {
 }
 
 function createUser(sub, usr_data) {
+    try {
+        // Insert a new document into the "users" collection
+        const result =  userscollection.insertOne({
+          sub: sub,
+          'user-data': usr_data,
+          'ns-data': null,
+        });
     
+        console.log(`Document created with ID ${result.insertedId}`);
+      } catch (err) {
+        console.error(err);
+      } 
 }
 
 function getNewsStacks(sub) {
