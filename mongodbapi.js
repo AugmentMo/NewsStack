ee925717968198d807bfd25bbd316a7a573c8d40
AugmentMo <moritz@ahlab.org>
2023-05-05 01:02:07 +1200

    added func stubs

---
+++ mongodbapi.js
@@ -0,0 +1,23 @@
+function isUserExisting(sub) {
+    
+}
+
+function getUserData(sub) {
+    
+}
+
+function updateUserData(sub, usr_data) {
+    
+}
+
+function createUser(sub, usr_data) {
+    
+}
+
+function getNewsStacks(sub) {
+    
+}
+
+function updateNewsStacks(sub, ns_data) {
+    
+}
---
+++ usersession-server.js
@@ -0,0 +1,22 @@
+// Users sessions
+// Array of dicts: {sid: string, socketid: string}
+var usersessions = []
+
+function isUserSessionExisting(sid) {
+    
+}
+
+function createUserSession(sid) {
+}
+
+
+function updateUserSessionSocketID(sid, socketid) {
+    
+}
+
+function removeUserSession(sid) {
+    
+}
+
+function getUserSessionSID(socketid) {
+}
