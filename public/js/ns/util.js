606f4cb9109e23d4f93cdc9f22d13394fffe0e02
AugmentMo <moritz@ahlab.org>
2023-05-08 23:42:04 +1200

    set and get cookie

--- public/js/ns/util.js
+++ public/js/ns/util.js
@@ -8,3 +8,28 @@ function generateRandomString(length) {
     return result;
   }
   
+
+  // Function to set a cookie with a given name and value
+function setCookie(cookieName, cookieValue, expirationDays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+// Function to get the value of a cookie with a given name
+function getCookie(cookieName) {
+  var name = cookieName + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var cookieArray = decodedCookie.split(';');
+  for(var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while (cookie.charAt(0) == ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(name) == 0) {
+      return cookie.substring(name.length, cookie.length);
+    }
+  }
+  return "";
+}
