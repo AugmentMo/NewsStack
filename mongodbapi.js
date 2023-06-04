0e0cca8bc7f2a6313fa58916ffd024143efc8be2
AugmentMo <moritz@ahlab.org>
2023-06-04 12:17:53 +1200

    load env variables

--- mongodbapi.js
+++ mongodbapi.js
@@ -1,5 +1,8 @@
 const { MongoClient } = require('mongodb');
 
+// Load environment variables from .env file
+require('dotenv').config();
+
 // Connection URI for MongoDB
 var mongodbUri = 'mongodb://localhost:27017'
 if (process.env.MONGODB_URI != undefined) {
@@ -42,78 +45,90 @@ async function connectMongoDB() {
 }
 
 function closeMongoDBConnection() {
-    // Close the client connection
-    mongodbClient.close();
-    mongodbconnected = false;
+    if (mongodbconnected){
+        // Close the client connection
+        mongodbClient.close();
+        mongodbconnected = false;
+    }
 }
 
 async function isUserExisting(sub) {
-    const result = await userscollection.findOne({ _id: sub })
+    if (mongodbconnected) {
+        const result = await userscollection.findOne({ _id: sub })
 
-    if (result) {
-        console.log("isUserExisting: found user")
-        return true;
-    } else {
-        console.log("isUserExisting: did not find user")
-        return false;
+        if (result) {
+            console.log("isUserExisting: found user")
+            return true;
+        } else {
+            console.log("isUserExisting: did not find user")
+            return false;
+        }
     }
 }
 
 async function createUser(sub, usr_data) {
-    const currentDate = new Date();
-    console.log("creating new user")
-    try {
-        // Insert a new document into the "users" collection
-        const result = await userscollection.insertOne({
-            _id: sub,
-            lastLogin: currentDate.toString(),
-            'user-data': usr_data,
-            'ns-data': null,
-        });
+    if (mongodbconnected) {
+        const currentDate = new Date();
+        console.log("creating new user")
+        try {
+            // Insert a new document into the "users" collection
+            const result = await userscollection.insertOne({
+                _id: sub,
+                lastLogin: currentDate.toString(),
+                'user-data': usr_data,
+                'ns-data': null,
+            });
     
-        console.log(`Document created with ID ${result.insertedId}`);
-      } catch (err) {
-        console.error(err);
-      } 
+            console.log(`Document created with ID ${result.insertedId}`);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 }
 
 async function getNewsStacks(sub) {
-    const filter = { _id: sub };
-    const user = await userscollection.findOne(filter);
+    if (mongodbconnected) {
+        const filter = { _id: sub };
+        const user = await userscollection.findOne(filter);
     
-    if (user) {
-        const ns_data = user['ns-data'];
-        return ns_data;
-    } else {
-        console.log('User with sub ' + sub + ' not found');
+        if (user) {
+            const ns_data = user['ns-data'];
+            return ns_data;
+        } else {
+            console.log('User with sub ' + sub + ' not found');
+        }
     }
 
     return null;
 }
 
 async function updateNewsStacks(sub, ns_data) {
-    const filter = { _id: sub };
-    const update = { $set: { 'ns-data': ns_data } };
+    if (mongodbconnected) {
+        const filter = { _id: sub };
+        const update = { $set: { 'ns-data': ns_data } };
 
-    const result = await userscollection.updateOne(filter, update);
+        const result = await userscollection.updateOne(filter, update);
 
-    if (result.modifiedCount === 1) {
-        console.log('ns-data field updated for user with sub ' + sub);
-    } else {
-        console.log('Error: User with sub ' + sub + ' not found');
+        if (result.modifiedCount === 1) {
+            console.log('ns-data field updated for user with sub ' + sub);
+        } else {
+            console.log('Error: User with sub ' + sub + ' not found');
+        }
     }
 }
 
 
 async function getUserData(sub) {
-    const filter = { _id: sub };
-    const user = await userscollection.findOne(filter);
+    if (mongodbconnected) {
+        const filter = { _id: sub };
+        const user = await userscollection.findOne(filter);
 
-    if (user) {
-        const user_data = user['user-data'];
-        return user_data;
-    } else {
-        console.log('User with sub ' + sub + ' not found');
+        if (user) {
+            const user_data = user['user-data'];
+            return user_data;
+        } else {
+            console.log('User with sub ' + sub + ' not found');
+        }
     }
 
     return null;
@@ -121,34 +136,38 @@ async function getUserData(sub) {
 
 
 async function updateUserData(sub, user_data) {
-    if (await isUserExisting(sub)) {
-        await createUser(sub, user_data);
-    }
-    else {
-        const filter = { _id: sub };
-        const update = { $set: { 'user-data': user_data } };
+    if (mongodbconnected) {
+        if (await isUserExisting(sub)) {
+            await createUser(sub, user_data);
+        }
+        else {
+            const filter = { _id: sub };
+            const update = { $set: { 'user-data': user_data } };
 
-        const result = await userscollection.updateOne(filter, update);
+            const result = await userscollection.updateOne(filter, update);
 
-        if (result.modifiedCount === 1) {
-            console.log('user-data field updated for user with sub ' + sub);
-        } else {
-            console.log('Error: User with sub ' + sub + ' not found');
+            if (result.modifiedCount === 1) {
+                console.log('user-data field updated for user with sub ' + sub);
+            } else {
+                console.log('Error: User with sub ' + sub + ' not found');
+            }
         }
     }
 }
 
 async function updateLastLogin(sub) {
-    const currentDate = new Date();
-    const filter = { _id: sub };
-    const update = { $set: { lastLogin: currentDate.toString() } };
+    if (mongodbconnected) {
+        const currentDate = new Date();
+        const filter = { _id: sub };
+        const update = { $set: { lastLogin: currentDate.toString() } };
 
-    const result = await userscollection.updateOne(filter, update);
+        const result = await userscollection.updateOne(filter, update);
 
-    if (result.modifiedCount === 1) {
-        console.log('lastLogin field updated for user with sub ' + sub);
-    } else {
-        console.log('Error: User with sub ' + sub + ' not found');
+        if (result.modifiedCount === 1) {
+            console.log('lastLogin field updated for user with sub ' + sub);
+        } else {
+            console.log('Error: User with sub ' + sub + ' not found');
+        }
     }
 }
 
